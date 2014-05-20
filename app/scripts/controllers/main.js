81bf2f911f962159517133f33d3712c3b8877acf
Shawn Leberknight <shawnleberknight@Shawns-MacBook-Pro.local>
2014-05-20 08:19:55 -0400

    toggle description and review tabs working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -25,7 +25,7 @@
 // 	};
 // });
 
-angular.module('shoppingCart')
+var app = angular.module('shoppingCart')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
@@ -39,30 +39,35 @@ angular.module('shoppingCart')
     	'this is something',
     	'more stuff'
     ];
-  })
+  });
 
-.controller('productCtrl', function($scope) {
+app.controller('productCtrl', function($scope) {
 	$scope.products = [
 		{
 			'thumbnail': 'http://cdn.roguefitness.com/media/catalog/product/cache/1/small_image/193x193/4c971525e32ff444db0f0150e765d17f/m/-/m-bruiser-th-new.jpg',
+			'pic': 'http://cdn.roguefitness.com/media/catalog/product/cache/1/header_image/0dc2d03fe217f8c83829496872af24a0/r/o/rogue-bruiser-bar-web-h1.jpg',
 			'name': 'Barbell',
 			'price': 150.00,
-			'description': 'American made steel olympic barbell. 45 lbs.'
+			'description': 'American made steel olympic barbell. 45 lbs.',
+			'review': 'Best bar out there!'
 		},
 		{
 			'thumbnail': 'http://cdn.roguefitness.com/media/catalog/product/cache/1/small_image/193x193/4c971525e32ff444db0f0150e765d17f/h/g/hg-bumpers-th-new.jpg',
+			'pic': 'http://cdn.roguefitness.com/media/catalog/product/cache/1/header_sm_image/0dc2d03fe217f8c83829496872af24a0/h/g/hg3ships.jpg',
 			'name': 'Bumber Plate Package',
 			'price': 500.00,
 			'description': '400 total pounds of American Made bumper weight plates.'
 		},
 		{
 			'thumbnail': 'http://cdn.roguefitness.com/media/catalog/product/cache/1/small_image/193x193/4c971525e32ff444db0f0150e765d17f/r/o/rogue-kettlebell-th.jpg',
+			'pic': 'http://cdn.roguefitness.com/media/catalog/product/cache/1/header_sm_image/0dc2d03fe217f8c83829496872af24a0/1/2/121-rogue-kettlebell-web-h1.jpg',
 			'name': 'Kettlebell',
 			'price': 50.00,
 			'description': 'American made 50 lb kettlebell.'
 		},
 		{
 			'thumbnail': 'http://cdn.roguefitness.com/media/catalog/product/cache/1/small_image/193x193/4c971525e32ff444db0f0150e765d17f/s/r/sr-1-rogue-bearing-speed-rope-web-th1.jpg',
+			'pic': 'http://cdn.roguefitness.com/media/catalog/product/cache/1/qv_header/0dc2d03fe217f8c83829496872af24a0/s/r/sr-1s-short-handle-bearing-speed-rope-web-h1.jpg',
 			'name': 'Speed Rope',
 			'price': 20.00,
 			'description': 'Speed jump rope. Perfect for double-unders!'
@@ -72,6 +77,17 @@ angular.module('shoppingCart')
 	// $scope.addData = function(number) {
 	// 	$scope.demoData.push(number);
 	// };
+});
+
+app.controller("PanelController", function() {
+	this.tab = 0;
+
+	this.selectTab = function(setTab) {
+		this.tab = setTab;
+	};
+	this.isSelected = function(checkTab) {
+		return this.tab === checkTab;
+	};
 });
 
 
