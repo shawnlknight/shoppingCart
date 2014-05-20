bf4419e506b0ada94b29cf7d4e1e0609e4bc4047
Shawn Leberknight <shawnleberknight@Shawns-MacBook-Pro.local>
2014-05-20 09:38:27 -0400

    clear review anddescription section

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -51,39 +51,43 @@ app.controller('productCtrl', function($scope) {
 			'description': 'American made steel olympic barbell. 45 lbs.',
 			'reviews': [
 				{
-					stars: 5,
-					body: "Best bar on the market!",
-					author: "johnSmith@something.com"
+					'stars': 5,
+					'body': "Best bar on the market!",
+					'author': "johnSmith@something.com"
 				}
 
-			]
+			],
+			_id: 0
 		},
 		{
 			'thumbnail': 'http://cdn.roguefitness.com/media/catalog/product/cache/1/small_image/193x193/4c971525e32ff444db0f0150e765d17f/h/g/hg-bumpers-th-new.jpg',
 			'pic': 'http://cdn.roguefitness.com/media/catalog/product/cache/1/header_sm_image/0dc2d03fe217f8c83829496872af24a0/h/g/hg3ships.jpg',
 			'name': 'Bumber Plate Package',
 			'price': 500.00,
-			'description': '400 total pounds of American Made bumper weight plates.'
+			'description': '400 total pounds of American Made bumper weight plates.',
+			_id: 1
 		},
 		{
 			'thumbnail': 'http://cdn.roguefitness.com/media/catalog/product/cache/1/small_image/193x193/4c971525e32ff444db0f0150e765d17f/r/o/rogue-kettlebell-th.jpg',
 			'pic': 'http://cdn.roguefitness.com/media/catalog/product/cache/1/header_sm_image/0dc2d03fe217f8c83829496872af24a0/1/2/121-rogue-kettlebell-web-h1.jpg',
 			'name': 'Kettlebell',
 			'price': 50.00,
-			'description': 'American made 50 lb kettlebell.'
+			'description': 'American made 50 lb kettlebell.',
+			_id: 2
 		},
 		{
 			'thumbnail': 'http://cdn.roguefitness.com/media/catalog/product/cache/1/small_image/193x193/4c971525e32ff444db0f0150e765d17f/s/r/sr-1-rogue-bearing-speed-rope-web-th1.jpg',
 			'pic': 'http://cdn.roguefitness.com/media/catalog/product/cache/1/qv_header/0dc2d03fe217f8c83829496872af24a0/s/r/sr-1s-short-handle-bearing-speed-rope-web-h1.jpg',
 			'name': 'Speed Rope',
 			'price': 20.00,
-			'description': 'Speed jump rope. Perfect for double-unders!'
+			'description': 'Speed jump rope. Perfect for double-unders!',
+			_id: 3
 		}
 		
 	]; 
-	// $scope.addData = function(number) {
-	// 	$scope.demoData.push(number);
-	// };
+	$scope.addData = function(item) {
+		$scope.product.reviews.push(item);
+	};
 });
 
 app.controller("PanelController", function() {
